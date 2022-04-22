26675b5940888663685753366698a08cc68bea28
vodinhhuy0911 <73160117+vodinhhuy0911@users.noreply.github.com>
2022-04-22 17:11:53 +0700

    delete file

--- React/webenglish/src/reducers/RootReducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import userReducers from "./UserReducers";
-
-const mainReducers = combineReducers({
-    'user': userReducers,
-})
-
-export default mainReducers
