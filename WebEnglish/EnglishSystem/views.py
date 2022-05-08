f3b96880485789502aa6ebf4f9a88757dd613488
vodinhhuy0911 <73160117+vodinhhuy0911@users.noreply.github.com>
2022-05-08 23:43:09 +0700

    fix score

--- WebEnglish/EnglishSystem/views.py
+++ WebEnglish/EnglishSystem/views.py
@@ -451,10 +451,13 @@ def test(request):
             result = 0.0
             for pk in question_pk:
                 choice_pk = request.POST.get('choice_pk-'+str(pk))
-                is_answer = Choice.objects.get(pk=choice_pk)
-                if is_answer.is_correct == True:
-                    mask = Question.objects.get(pk=pk)
-                    result += float(mask.maximum_marks)
+                try:
+                    is_answer = Choice.objects.get(pk=choice_pk)
+                    if is_answer.is_correct == True:
+                        mask = Question.objects.get(pk=pk)
+                        result += float(mask.maximum_marks)
+                except:
+                    result += 0.0
             context = {
                 'question_pk':question_pk,
                 'link': ' /test',
@@ -534,10 +537,13 @@ def test_reading_comprehension(request):
             for id in question:
                 choice_pk = request.POST.get('choice_pk-'+str(id))
                 print(choice_pk)
-                is_answer = Choice.objects.get(pk=choice_pk)
-                if is_answer.is_correct == True:
-                    mask = Question.objects.get(pk=id)
-                    result += float(mask.maximum_marks)
+                try:
+                    is_answer = Choice.objects.get(pk=choice_pk)
+                    if is_answer.is_correct == True:
+                        mask = Question.objects.get(pk=id)
+                        result += float(mask.maximum_marks)
+                except:
+                    result += 0.0
             context = {
                 'result':result,
                 'link': ' /test-reading-comprehension'
@@ -589,41 +595,68 @@ def test_reading_comprehension(request):
 @login_required()
 def test_incomplete_text(request):
     if request.method == 'POST':
-        paragraph_pk = request.POST.get('paragraph_pk')
-        question = request.POST.getlist('question_pk')
-        print(question)
-        result = 0.0
-        for id in question:
-            choice_pk = request.POST.get('choice_pk-'+str(id))
-            print(choice_pk)
-            is_answer = Choice.objects.get(pk=choice_pk)
-            if is_answer.is_correct == True:
-                mask = Question.objects.get(pk=id)
-                result += float(mask.maximum_marks)
-        context = {
-            'result':result,
-            'link': ' /test-incomplete-text'
-        }
-        return render(request,'quiz/result.html',context)
+        if 'submit' in request.POST:
+            paragraph_pk = request.POST.get('paragraph_pk')
+            question = request.POST.getlist('question_pk')
+            print(question)
+            result = 0.0
+            for id in question:
+                choice_pk = request.POST.get('choice_pk-' + str(id))
+                print(choice_pk)
+                try:
+                    is_answer = Choice.objects.get(pk=choice_pk)
+                    if is_answer.is_correct == True:
+                        mask = Question.objects.get(pk=id)
+                        result += float(mask.maximum_marks)
+                except:
+                    result += 0.0
+            context = {
+                'result': result,
+                'link': ' /test-incomplete-text'
+            }
+            return render(request, 'quiz/result.html', context)
+        if 'help' in request.POST:
+            list_para = []
+            result = []
+            list_para.append(Paragraph.objects.get(pk=request.POST.get('paragraph_pk')))
+            list_question = []
+            for para in list_para[:1]:
+                # list_question.append(Question.objects.get(pk=request.POST.get('qid')))
+                list_question = list(Question.objects.filter(paragraph=para))
+                for question in list_question:
+                    c = list(Choice.objects.filter(question=question))
+                    input = para.content + "\n\nQuestions:\n" + question.html + "__" + c[0].html + "__" + c[
+                        1].html + "__" + \
+                            c[2].html + "__" + c[3].html
+                    if question == Question.objects.get(pk=request.POST.get('qid')):
+                        print(request.POST.get('qid'))
+                        result.append(incom_text.incomplete_text(input))
+                    else:
+                        result.append(" ")
+            context = {
+                'list_para': (list_para[:1]),
+                'list_question': zip(list_question, result),
+                'flag': 1
+            }
+            return render(request, 'quiz/test_reading_comprehension.html', context=context)
     else:
         list_para = list(Paragraph.objects.filter(type='2'))
         random.shuffle(list_para)
         result = []
         list_question = []
         for para in list_para[:1]:
-            list_question = list(Question.objects.filter(paragraph = para))
-            # for question in list_question:
-            #     c = list(Choice.objects.filter(question = question))
-            #     input = para.content + "\n\nQuestions:\n" + question.html+ "__" + c[0].html+ "__" + c[1].html+ "__"+ c[2].html+ "__"+ c[3].html
-            #     result.append(reading_compre.reading_comprehension(input))
-            # list_percent =
+            list_question = list(Question.objects.filter(paragraph=para))
+            for question in list_question:
+                c = list(Choice.objects.filter(question=question))
+                input = para.content + "\n\nQuestions:\n" + question.html + "__" + c[0].html + "__" + c[1].html + "__" + \
+                        c[2].html + "__" + c[3].html
+                result.append(input)
         print(list_para)
-        print(list_question)
         context = {
             'list_para': (list_para[:1]),
-            'list_question': list_question
+            'list_question': zip(list_question, result),
+            'flag': 0
         }
-        # print(context["input"][0])
         return render(request, 'quiz/test_reading_comprehension.html', context=context)
 
 
