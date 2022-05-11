70394755335ec7445e78e7fb7c382a4166080d9e
vodinhhuy0911 <73160117+vodinhhuy0911@users.noreply.github.com>
2022-05-11 12:21:13 +0700

    chinh sua hinh anh

--- WebEnglish/EnglishSystem/admin.py
+++ WebEnglish/EnglishSystem/admin.py
@@ -48,17 +48,9 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Question, QuestionAdmin)
-
-
-#
-
-# admin.site.register(Choice)
-# admin.site.register(QuizProfile)
 admin.site.register(AttemptedQuestion)
-
 admin.site.register(Choice)
 admin.site.register(QuizProfile)
-# admin.site.register(AttemptedQuestion)
 admin.site.register(Paragraph)
 admin.site.register(Category)
 admin.site.register(Post,PostAdmin)
